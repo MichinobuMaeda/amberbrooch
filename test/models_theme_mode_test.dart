93c04714ac77398e09f8db6fd64e706b77278ef5
Michinobu Maeda <michinobumaeda@gmail.com>
2021-10-02 22:54:29 +0900

    bug fix

--- test/models_theme_mode_test.dart
+++ test/models_theme_mode_test.dart
@@ -4,12 +4,12 @@ import 'package:amberbrooch/main.dart';
 
 void main() {
   test('entities/clientModel: initialized with defaultThemeMode', () {
-    ClientModel clientModel = ClientModel();
+    ClientModel clientModel = ClientModel(WebClientLocalStore());
     expect(clientModel.themeMode, defaultThemeMode);
   });
   test('entities/clientModel: can get/set mode.', () {
     ThemeMode themeMode = ThemeMode.dark;
-    ClientModel clientModel = ClientModel();
+    ClientModel clientModel = ClientModel(WebClientLocalStore());
     clientModel.themeMode = themeMode;
     expect(clientModel.themeMode, themeMode);
     expect(clientModel.themeMode == defaultThemeMode, false);
